745896ee357e05560fede7236a4ba9f53ffb3778
Jhow Paes <jhow.paes@coderoom.com.br>
2019-08-22 17:52:29 -0300

    added notification to appointment

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,9 +1,12 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
+
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
   async index(req, res) {
@@ -60,6 +63,15 @@ class AppointmentController {
     }
 
     /**
+     * Check if the user is not the provider
+     */
+    if (provider_id === req.userId) {
+      return res
+        .status(401)
+        .json({ error: 'you cannot schedule a service for yourself' });
+    }
+
+    /**
      * Check for past dates
      */
     const hourStart = startOfHour(parseISO(date));
@@ -89,6 +101,23 @@ class AppointmentController {
       user_id: req.userId,
       provider_id,
       date,
+    });
+
+    /**
+     * Notify appointment provider
+     */
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', às' H:mm'h'",
+      {
+        locale: pt,
+      }
+    );
+
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para ${formattedDate}`,
+      user: provider_id,
     });
 
     return res.json(appointment);
