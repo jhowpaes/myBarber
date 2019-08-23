8f9b6f9bd5b06075121f10d7cc7d99c1731425a5
Jhow Paes <jhow.paes@coderoom.com.br>
2019-08-23 13:35:38 -0300

    implement nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '503b0fa2331800',
+    pass: '9028ca370267a9',
+  },
+  default: {
+    from: 'Equipe MyBarber <noreply@coderoom.com.br>',
+  },
+};
